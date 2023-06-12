fc9875383281d44ad5f8a63a04ae95443ead31ab
Hans Lorico <hqlorico@up.edu.ph>
2023-06-12 12:24:51 +0800

    feature: transfer the location of python flask app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 
-import NavBar from './components/navbar';
-import CenterCard from './components/centerCard';
-import Footer from './components/footer';
-import { Box } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// pages
+import Home from './pages/home';
+import About from './pages/about';
 
 
 function App() {
   return (
-    <div>
-      <Box height="100vh" display="flex" flexDirection="column">
-        <NavBar />
-        <CenterCard />
-        <Footer/>
-      </Box>
-    </div>
+    <>
+      <Router>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import { Box } from '@mui/material';
+import React from 'react'
+
+// components
+import NavBar from '../components/navbar';
+import CenterCard from '../components/centerCard';
+import Footer from '../components/footer';
+
+function About() {
+    return ( 
+    <div>
+        <Box height="100vh" display="flex" flexDirection="column">
+          <NavBar />
+          {/* <CenterCard /> */}
+          <Footer/>
+        </Box>
+    </div>
+    );
+}
+
+export default About;
